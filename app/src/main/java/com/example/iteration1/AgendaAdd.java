06890b06b5d2984c25d59337662810fd16fd7117
unknown <jhdq6b@mail.umkc.eduEve1stChaos>
2020-03-08 08:00:30 -0500

    Partial update to code without crashes

--- app/src/main/java/com/example/iteration1/AgendaAdd.java
+++ app/src/main/java/com/example/iteration1/AgendaAdd.java
@@ -1,8 +1,15 @@
 package com.example.iteration1;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.DatePicker;
+
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
 
 public class AgendaAdd extends Activity {
 
@@ -12,6 +19,26 @@ public class AgendaAdd extends Activity {
         setContentView(R.layout.agenda_adding);
         DisplayMetrics AgendaAddingDM = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(AgendaAddingDM);
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+        }
     }
 }
 
