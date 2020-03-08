451dee060fadd8c325767755c4474484b67e29ff
unknown <jhdq6b@mail.umkc.eduEve1stChaos>
2020-03-08 08:46:57 -0500

    Updating Files Date Picker Works as intended

--- app/src/main/java/com/example/iteration1/AgendaAdd.java
+++ app/src/main/java/com/example/iteration1/AgendaAdd.java
@@ -1,45 +1,62 @@
 package com.example.iteration1;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentActivity;
 
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
 import java.util.Calendar;
 
-public class AgendaAdd extends Activity {
+public class AgendaAdd extends FragmentActivity implements DatePickerDialog.OnDateSetListener {
 
     @Override
-    protected void onCreate (Bundle savedInstance) {
+    protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.agenda_adding);
         DisplayMetrics AgendaAddingDM = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(AgendaAddingDM);
-    }
 
-    public static class DatePickerFragment extends DialogFragment
-            implements DatePickerDialog.OnDateSetListener {
+        Button set_Date = findViewById(R.id.date_Select);
+        set_Date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
 
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current date as the default date in the picker
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
 
-            // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
         }
 
-        public void onDateSet(DatePicker view, int year, int month, int day) {
-            // Do something with the date chosen by the user
-        }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String current_Date_Set = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+
+        TextView set_Date = findViewById(R.id.view_Date);
+        set_Date.setText(current_Date_Set);
     }
 }
+
+
+
 
 
