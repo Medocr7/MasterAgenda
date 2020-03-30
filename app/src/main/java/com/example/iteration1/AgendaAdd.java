169c68beb1028b36820060a027121d8a42f5c6c0
User <User@DESKTOP-DVIF9J0>
2020-03-29 22:38:17 -0500

    MasterAgenda ability to store texts

--- app/src/main/java/com/example/iteration1/AgendaAdd.java
+++ app/src/main/java/com/example/iteration1/AgendaAdd.java
@@ -5,11 +5,17 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.iteration1.DataModels.AgendaItem;
+import com.example.iteration1.SQLiteHelper.DatabaseHandler;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -23,6 +29,11 @@ import java.util.Calendar;
 
 public class AgendaAdd extends FragmentActivity implements DatePickerDialog.OnDateSetListener {
 
+
+    DatabaseHandler dbHandler;
+    EditText add_Item_Name;
+    TextView set_Date;
+
     @Override
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
@@ -30,8 +41,15 @@ public class AgendaAdd extends FragmentActivity implements DatePickerDialog.OnDa
         DisplayMetrics AgendaAddingDM = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(AgendaAddingDM);
 
-        Button set_Date = findViewById(R.id.date_Select);
-        set_Date.setOnClickListener(new View.OnClickListener() {
+
+        dbHandler = new DatabaseHandler(getApplicationContext());
+        add_Item_Name = findViewById(R.id.add_Item_Name);
+        set_Date = findViewById(R.id.view_Date);
+
+
+
+        Button date_Select = findViewById(R.id.date_Select);
+        date_Select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DialogFragment datePicker = new DatePickerFragment();
@@ -40,7 +58,26 @@ public class AgendaAdd extends FragmentActivity implements DatePickerDialog.OnDa
         });
 
 
-        }
+        //Add Agenda
+        Button Add_Item = findViewById(R.id.Add_Item);
+        Add_Item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(!TextUtils.isEmpty(add_Item_Name.getText().toString()) && !set_Date.getText().toString().contains("Date")){
+                    dbHandler.addAgenda(new AgendaItem(add_Item_Name.getText().toString(),set_Date.getText().toString() ));
+                    Toast.makeText(getApplicationContext(),"Agenda Has been added Successfully",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"Enter the Agenda Name and also Pick the Date",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),"Enter the Agenda Name and also Pick the Date",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+
+    }
+
 
 
     @Override
@@ -51,7 +88,7 @@ public class AgendaAdd extends FragmentActivity implements DatePickerDialog.OnDa
         c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
         String current_Date_Set = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
 
-        TextView set_Date = findViewById(R.id.view_Date);
+
         set_Date.setText(current_Date_Set);
     }
 }
--- app/src/main/java/com/example/iteration1/MainActivity.java
+++ app/src/main/java/com/example/iteration1/MainActivity.java
@@ -8,49 +8,75 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.Toast;
+
+import com.example.iteration1.SQLiteHelper.DatabaseHandler;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // This button will view all the Plans/Task that the user has added into the Agenda
-    Button ViewTask = findViewById(R.id.Agenda);
-    ViewTask.setOnClickListener(new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            startActivity(new Intent(MainActivity.this, AgendaViewing.class));
-        }
-    });
+
+
+
+
+        dbHandler = new DatabaseHandler(getApplicationContext());
+
+        // This button will view all the Plans/Task that the user has added into the Agenda
+        Button ViewTask = findViewById(R.id.Agenda);
+        ViewTask.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //View all entered Agends
+                dbHandler.getAllAgendaItems();
+                if(DatabaseHandler.agendaList.size()>0){
+                    for(int i=0;i < DatabaseHandler.agendaList.size(); i++){
+                        Toast.makeText(MainActivity.this,"Agenda Name: " + DatabaseHandler.agendaList.get(i).getAgendaName() +
+                                "Agenda Date: " + DatabaseHandler.agendaList.get(i).getAgendaDate(),Toast.LENGTH_LONG).show();
+                    }
+
+                }else{
+                    Toast.makeText(MainActivity.this,"No Agenda List Found",Toast.LENGTH_LONG).show();
+                }
+
+                startActivity(new Intent(MainActivity.this, AgendaViewing.class));
+
+
+            }
+        });
     /* This button will pop up a menu that will allow the user to add items into a date. Which will then be recorded and pulled up should the user ever
     exit the application.
     */
-    Button AddPlans = findViewById(R.id.AgendaAdd);
-    AddPlans.setOnClickListener(new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            startActivity(new Intent(MainActivity.this, AgendaAdd.class));
-        }
-    });
-    // This button will pop up a view of the calender of the month ***TRACKS BASED ON REAL TIME***
-    Button CalenderView = findViewById(R.id.CalenderView);
-    CalenderView.setOnClickListener(new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            startActivity(new Intent(MainActivity.this, ViewCalender.class));
-        }
-    } );
+        Button AddPlans = findViewById(R.id.AgendaAdd);
+        AddPlans.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AgendaAdd.class));
+            }
+        });
+        // This button will pop up a view of the calender of the month ***TRACKS BASED ON REAL TIME***
+        Button CalenderView = findViewById(R.id.CalenderView);
+        CalenderView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ViewCalender.class));
+            }
+        });
 
-    // Exits the App and pops with an error code of 0.
-    Button ExitApp =findViewById(R.id.ExitApp);
-    ExitApp.setOnClickListener(new OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            finish();
-            System.exit(0);
-        }
-    });
+        // Exits the App and pops with an error code of 0.
+        Button ExitApp = findViewById(R.id.ExitApp);
+        ExitApp.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
     }
 }
